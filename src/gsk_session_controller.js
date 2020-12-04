39bf21a8080487ddd4253e8ff929a3e42ceed073
David Gascueña <davidgasku@gmail.com>
2020-12-04 22:24:47 +0100

    restored gsk_sesion_controller.js

--- src/gsk_session_controller.js
+++ src/gsk_session_controller.js
@@ -1,18 +1,33 @@
 const gsk_DEBUG = true;
 
-var GSK = {};
-GSK.session_controller = {};
+GSK = {};
 
-GSK.session_controller.init = function (ide) {
+GSK.init = function(broadcast) {
     if (gsk_DEBUG) console.log("gks_session.init()");
-    GSK.session_controller.ide = ide;
+
+    GSK.ide = window.world.children[0];
+    GSK.broadcast = broadcast;
+
     ide.flatDesign();
-    //ide.palette.toggleVisibility();
-    //ide.stage.toggleVisibility()
-    //ide.stageHandle.toggleVisibility()
+    ide.palette.toggleVisibility();
+    ide.stage.toggleVisibility();
+    ide.stageHandle.toggleVisibility();
+
+    SyntaxElementMorph.prototype.fontSize = 12;
+    CommandBlockMorph.prototype.corner = 6;
 
     window.onbeforeunload = nop;
-    IDE_Morph.prototype.set
+    return GSK;
+}
+
+GSK.onPublishReceived = function (d, s) {
+    console.log(d, s);
+
+    let sprite = ide.sprites.contents.find(s => s.name === d.o);
+    if (sprite) {
+        sprite.gotoXY(d.x, d.y);
+        sprite.setHeading( d.heading );
+    }
 }
 
 GSK.setTopBarVisibility = function (bool) {

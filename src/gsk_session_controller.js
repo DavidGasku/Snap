de1cc525dbfc44aed9719e6355b12f1609f4dff6
David Gascueña <davidgasku@gmail.com>
2020-12-07 23:10:21 +0100

    clean up

--- src/gsk_session_controller.js
+++ src/gsk_session_controller.js
@@ -26,18 +26,8 @@ GSK = {
 };
 
 GSK.init = function(broadcast) {
-    if (gsk_DEBUG) console.log("gks_session.init()");
-
     GSK.ide = window.world.children[0];
     GSK.broadcast = broadcast;
-
-    // ide.flatDesign();
-    // ide.palette.toggleVisibility();
-    // ide.stage.toggleVisibility();
-    // ide.stageHandle.toggleVisibility();
-
-    //SyntaxElementMorph.prototype.fontSize = 12;
-    //CommandBlockMorph.prototype.corner = 6;
 
     window.onbeforeunload = nop;
     return GSK;
